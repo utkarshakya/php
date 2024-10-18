60ea99fc1283519632e4996c4d0d725411116d52
Utkarsh Shakya <subhashshakya136@gmail.com>
2024-10-18 15:29:22 +0530

    ADDED 01_basics\02_variable.php FILE

---
+++ 01_basics/02_variable.php
@@ -0,0 +1,12 @@
+<?php
+// Variables in php are starts with $ sign.
+// All other rules of defining a variable are same as in other high level languages.
+
+$name = "Danny Rand";
+echo $name;
+// See the output in the browser after running the Apache Server (I hope you remember that, you always have to start apache first),
+// And then go to localhost/file_relative_path (relative to htdocs which is localhost).
+
+// There is no need to define the data type while declaring variables unlike C language.
+// php is more like a Javascript where we just need to use let to define a variable, here its $. That't it.
+?>
