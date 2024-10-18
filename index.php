3c8f8b1b3c06c5334e62f6b05e42804cdfc65546
Utkarsh Shakya <subhashshakya136@gmail.com>
2024-10-18 10:01:52 +0530

    ADDED index.php FILE

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+echo "Hola Utkarsh"
+?>
+
+<!--
+Now Go To Your Browser And Type localhost/ (this will by default run the index.php file).
+If You Made Any Folder Then Type localhost/folderName/fileName.
+This will run the php file on Apache Server. And You made you first php Program.
+ -->
