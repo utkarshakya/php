d241a0990f03c38f1408b6e9a41c3d338a80ef59
Utkarsh Shakya <subhashshakya136@gmail.com>
2024-10-18 10:31:03 +0530

    ADDED 01_basics\01_basics.php FILE

---
+++ 01_basics/01_basics.php
@@ -0,0 +1,21 @@
+<!-- We use php with html and we can also use it in parts as you can see below -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <?php
+    echo "Hola Amigo.<br>"; // This is br tag of html.
+    echo "My Name is Utkarsh <br>";
+    ?>
+</body>
+<?php
+echo "They can you me anywhere in the page";
+?>
+</html>
+
+<!-- Run this file in your browser using the localhost/fileLocation -->
