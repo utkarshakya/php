7b5009143f5d0c112b771f0d73a2b097a70db255
Utkarsh Shakya <subhashshakya136@gmail.com>
2024-10-20 09:27:31 +0530

    ADDED 01_basics\04_operators.php FILE

---
+++ 01_basics/04_operators.php
@@ -0,0 +1,15 @@
+<?php
+// Like All Other Languages php Also Has Same Basic Operator
+// 1. Arithmetic Operator (+, -, *, /, %), it also has ** (exponential operator)
+// 2. Assignment Operator (=, +=, -=, *=, /=, %=, **=, etc)
+// 3. Comparison Operator (==, <, >, <=, >=, !=, <>)
+// 4. Logical Operator (&&, ||) or (AND, OR).
+// 5. And Many More, php is keep evolving it also has new operators like the Nullish Coalescing Operator, etc.
+
+$num1 = 3;
+$num2 = 2;
+
+echo "3 ^ 2 = ".($num1**$num2); // Here you can see the . which is also an operator.
+
+// There is so much to talk on operators but we'll learn them as we go through.
+// Check All Here: https://www.php.net/manual/en/language.operators.php
