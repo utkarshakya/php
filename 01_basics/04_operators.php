7dfef140f12ffbf72227c1065b966699c3f57b00
Utkarsh Shakya <subhashshakya136@gmail.com>
2024-10-20 10:26:07 +0530

    UPDATED 01_basics\04_operators.php FILE

--- 01_basics/04_operators.php
+++ 01_basics/04_operators.php
@@ -1,15 +1,63 @@
 <?php
-// Like All Other Languages php Also Has Same Basic Operator
-// 1. Arithmetic Operator (+, -, *, /, %), it also has ** (exponential operator)
-// 2. Assignment Operator (=, +=, -=, *=, /=, %=, **=, etc)
-// 3. Comparison Operator (==, <, >, <=, >=, !=, <>)
-// 4. Logical Operator (&&, ||) or (AND, OR).
-// 5. And Many More, php is keep evolving it also has new operators like the Nullish Coalescing Operator, etc.
+// Like All Other Languages PHP Also Has Same Basic Operator
+
+// Operators in PHP:
+//     |
+//     +-- Arithmetic Operators
+//     |       |
+//     |       +-- Addition (+)
+//     |       +-- Subtraction (-)
+//     |       +-- Multiplication (*)
+//     |       +-- Division (/)
+//     |       +-- Modulus (%)
+//     |
+//     +-- Assignment Operators
+//     |       |
+//     |       +-- Assignment (=)
+//     |       +-- Addition Assignment (+=)
+//     |       +-- Subtraction Assignment (-=)
+//     |       +-- Multiplication Assignment (*=)
+//     |       +-- Division Assignment (/=)
+//     |
+//     +-- Comparison Operators
+//     |       |
+//     |       +-- Equal (==)
+//     |       +-- Identical (===)
+//     |       +-- Not Equal (!=)
+//     |       +-- Not Identical (!==)
+//     |       +-- Greater Than (>)
+//     |       +-- Less Than (<)
+//     |
+//     +-- Increment/Decrement Operators
+//     |       |
+//     |       +-- Increment (++)
+//     |       +-- Decrement (--)
+//     |
+//     +-- Logical Operators
+//     |       |
+//     |       +-- And (&&)
+//     |       +-- Or (||)
+//     |       +-- Not (!)
+//     |
+//     +-- String Operators
+//     |       |
+//     |       +-- Concatenation (.)
+//     |       +-- Concatenation Assignment (.=)
+//     |
+//     +-- Array Operators
+//             |
+//             +-- Union (+)
+//             +-- Equality (==)
+//             +-- Identity (===)
+//             +-- Inequality (!=)
+//             +-- Non-Identity (!==)
+
+// And Many More, PHP Is Keep Evolving It Also Has New Operators Like The Nullish Coalescing Operator, etc.
 
 $num1 = 3;
 $num2 = 2;
-
-echo "3 ^ 2 = ".($num1**$num2); // Here you can see the . which is also an operator.
+echo "3 ^ 2 = ".($num1**$num2);
+// And ** is a exponential operator.
 
 // There is so much to talk on operators but we'll learn them as we go through.
 // Check All Here: https://www.php.net/manual/en/language.operators.php
