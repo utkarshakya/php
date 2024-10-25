3123a1f2e9bbf6bdd2fa198e2f6f66b4877f500a
Utkarsh Shakya <subhashshakya136@gmail.com>
2024-10-25 14:44:51 +0530

    UPDATED 02_database\03_create_database.php FILE

--- 02_database/03_create_database.php
+++ 02_database/03_create_database.php
@@ -6,23 +6,35 @@ $password = "";
 
 try {
     $conn = mysqli_connect($hostname, $username, $password);
-    echo "Connection Successful";
+    echo "Connection Created Successfully";
 } catch (mysqli_sql_exception $err) {
     echo "Sorry! ". mysqli_connect_error();
     die; // This will end the program if there is any error.
 }
 
-// Now We Can Create A Database.
+// --- NOW WE CAN CREATE A DATABASE ---
 // First we write the sql query.
-$sql = "CREATE DATABASE DEFENDERS";
+$sql = "CREATE DATABASE `The Defender`"; // Note using `` is good practice when you write any name instead of query.
 // And then we execute it.
 try {
-    $result = mysqli_query($conn, $sql);
+    mysqli_query($conn, $sql);
     // The query function return true or false.
     echo "<br>Database Created Successfully";
 } catch (mysqli_sql_exception $err) {
     echo "<br>Sorry! ". mysqli_error($conn);
 }
-
 // That's it we have created our database from php, Smile (:
+
+// --- NOW WE CAN ALSO DELETE A DATABASE ---
+/*
+$sqlQueryToDeleteDB = "DROP DATABASE `The Defender`";
+try {
+    mysqli_query($conn, $sqlQueryToDeleteDB);
+    echo "<br>Database Deleted Successfully";
+} catch (mysqli_sql_exception $err) {
+    echo "<br>Sorry! ". mysqli_error($conn);
+}
+*/
+
+mysqli_close($conn);
 ?>
