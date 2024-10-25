67761316597bf2c08462ad7e56e6344f2779ca04
Utkarsh Shakya <subhashshakya136@gmail.com>
2024-10-25 08:47:33 +0530

    ADDED 02_basics\01_form.php FILE

---
+++ 02_basics/submit.php
@@ -0,0 +1,6 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    echo "Logged In Successfully!";
+}
+// This Is How We Check The Request, When We Connect The Database Things Will Get More Real World.
+?>
