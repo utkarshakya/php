bddd770270b496744827657832abe85bf1f56323
Utkarsh Shakya <subhashshakya136@gmail.com>
2024-12-11 11:24:30 +0530

    Add 06_oops\05_inheritance.php

---
+++ 06_oops/05_inheritance.php
@@ -0,0 +1,22 @@
+<?php
+// Inheritance in PHP allows a class (child class) to acquire properties and methods from another class (parent class) using the extends keyword.
+
+class Vehicle {
+    public function honk() {
+        echo "Honk!";
+    }
+}
+
+// This is inherited class have the properties and methods of the Vehicle class.
+class Car extends Vehicle {
+    public function drive() {
+        echo "Driving...";
+    }
+}
+
+$myCar = new Car(); // Note here we create the object of Car class but we can access the method of Vehicle class
+$myCar->honk(); // Outputs: Honk!
+
+// This is the inheritance.
+// Also here with sub classes, Protected Access Modifier comes in play.
+?>
